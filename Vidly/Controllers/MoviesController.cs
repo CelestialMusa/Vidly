c1d585ad19b300118ac662894ec768dcf7b8ec85
CelestialMusa <Zakes.musa@hotmail.com>
2019-01-13 16:49:12 +0200

    Fix Entity framework validation errors when adding and editing a movie

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
+using Vidly.ViewModels;
+using System.Data.Entity.Validation;
 
 namespace Vidly.Controllers
 {
@@ -36,15 +38,69 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
+        public ViewResult New()
+        {
+            var genreTypes = _dbContext.GenreTypes.ToList();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                GenreTypes = genreTypes
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var movie = _dbContext.Movies.Single(m => m.Id == Id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                GenreTypes = _dbContext.GenreTypes.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                movie.GenreType = _dbContext.GenreTypes.Single(g => g.Id == movie.GenreType.Id);
+                _dbContext.Movies.Add(movie);
+            }
+            else
+            {
+                var editedMovie = _dbContext.Movies.Include(m => m.GenreType).Single(c => c.Id == movie.Id);
+
+                editedMovie.Name = movie.Name;
+                editedMovie.ReleaseDate = movie.ReleaseDate;
+                editedMovie.GenreType = _dbContext.GenreTypes.Single(g => g.Id == editedMovie.GenreType.Id);
+                editedMovie.NumberInStock = movie.NumberInStock;
+            }
+
+            try
+            {
+                _dbContext.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return RedirectToAction("Index", "Movies");
+        }
+
         [Route("Movies/Index/{pageIndex}/{sortBy}")]
         public ActionResult Index(int? pageIndex, string sortBy)
         {
             return Content(String.Format("PageIndex={0}&sortBy={1}", pageIndex ?? 1, String.IsNullOrWhiteSpace(sortBy) ? "Name" : sortBy));
-        }
-
-        public ActionResult edit(int id)
-        {
-            return Content("Id=" + id);
         }
 
         [Route("Movies/Released/{year:regex(\\d{4}):range(1970, 2018)}/{month:regex(\\d{2}):range(1,12)}")]
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,17 +9,24 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         [Required]
         [DisplayFormat(DataFormatString = "{0:dddd, MMMM dd yyyy}")]
+        [Display(Name ="Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         [Required]
         [DisplayFormat(DataFormatString = "{0:dddd, MMMM dd yyyy}")]
         public DateTime DateAdded { get; set; }
+
         [Required]
+        [Display(Name = "Number In Stock")]
         public int NumberInStock { get; set; }
+
         public GenreType GenreType { get; set; }
     }
 }
